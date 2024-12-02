42c6ecadad3a98d2bf27f8b2d50b26fbd4b2301d
Aramxxx8691 <khurshudyan2013@gmail.com>
2024-12-02 21:04:57 +0400

    Version_1.3 LTranslation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,50 +14,84 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
-      <div className="container">
-        <Link className="navbar-brand fw-bold fs-4" to="/">
-          LEA
-        </Link>
-        <button
-          className="navbar-toggler border-0"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded={!isCollapsed} // Control collapse state based on our state
-          aria-label="Toggle navigation"
-          onClick={toggleNavbar} // Toggle the collapse state when button is clicked
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link fs-5" aria-current="page" to="/">
-                {translations.home}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link fs-5" to="/about">
-                {translations.about}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link fs-5" to="/pricing">
-                {translations.pricing}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link fs-5" to="/contact">
-                {translations.contact}
-              </Link>
-            </li>
-          </ul>
+<nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
+  <div className="container">
+    {/* Left Section: Logo */}
+    <div className="d-flex align-items-center">
+      <Link className="navbar-brand fw-bold fs-4" to="/">
+        LEA
+      </Link>
+    </div>
+
+    {/* Mobile Toggle Button */}
+    <button
+      className="navbar-toggler border-0"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded={!isCollapsed} // Control collapse state
+      aria-label="Toggle navigation"
+      onClick={toggleNavbar} // Toggle the collapse state when button is clicked
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    {/* Right Section: Menu and LanguageSwitcher */}
+    <div
+      className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`}
+      id="navbarNav"
+    >
+      <div className="d-flex flex-column flex-lg-row align-items-center w-100">
+        {/* Centered Menu */}
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item">
+            <Link
+              className="nav-link fs-5"
+              aria-current="page"
+              to="/"
+              onClick={() => setIsCollapsed(true)}
+            >
+              {translations.home}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link fs-5"
+              to="/about"
+              onClick={() => setIsCollapsed(true)}
+            >
+              {translations.about}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link fs-5"
+              to="/pricing"
+              onClick={() => setIsCollapsed(true)}
+            >
+              {translations.pricing}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link fs-5"
+              to="/contact"
+              onClick={() => setIsCollapsed(true)}
+            >
+              {translations.contact}
+            </Link>
+          </li>
+        </ul>
+
+        {/* Language Switcher */}
+        <div className="d-flex justify-content-center justify-content-lg-end mt-3 mt-lg-0">
           <LanguageSwitcher />
         </div>
       </div>
-    </nav>
+    </div>
+  </div>
+</nav>
   );
 }
 
