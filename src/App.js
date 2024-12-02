2f5bdab9e90c8e71db6ef895477c2ff7494a48a5
Aramxxx8691 <khurshudyan2013@gmail.com>
2024-12-02 15:23:25 +0400

    Version_1.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -12,11 +12,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+const RedirectHandler = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const queryString = window.location.search; // e.g., "?courses/austria"
+    if (queryString) {
+      const path = queryString.slice(1); // Remove '?' to get "courses/austria"
+      navigate(`/${path}`); // Navigate to "/courses/austria"
+    }
+  }, [navigate]);
+
+  return null; // No UI for this component
+};
+
 function App() {
   return (
     <Router basename="/vns">
       <div className="App">
         <Navbar />
+        <RedirectHandler />
         <div className="mainContent">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -33,3 +48,4 @@ function App() {
 }
 
 export default App;
+
