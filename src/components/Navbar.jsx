e78391364ae452692bb7dba8caad473dcb33917b
Aramxxx8691 <khurshudyan2013@gmail.com>
2024-12-02 18:48:15 +0400

    Version_1.3 LTranslation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
-// src/components/Navbar.js
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { LanguageContext } from '../contexts/LanguageContext';
 import LanguageSwitcher from './LanguageSwitcher';
 
 function Navbar() {
-  const { translations } = useContext(LanguageContext); // Access the current translations
+  const { translations } = useContext(LanguageContext);
+  
+  // Add a state to control the navbar collapse
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleNavbar = () => {
+    setIsCollapsed(!isCollapsed); // Toggle collapse state
+  };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
@@ -19,12 +25,13 @@ function Navbar() {
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
           aria-controls="navbarNav"
-          aria-expanded="false"
+          aria-expanded={!isCollapsed} // Control collapse state based on our state
           aria-label="Toggle navigation"
+          onClick={toggleNavbar} // Toggle the collapse state when button is clicked
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link fs-5" aria-current="page" to="/">
